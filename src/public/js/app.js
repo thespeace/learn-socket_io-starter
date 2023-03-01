65f42f8ff24207948566113a88720d289bcd8e9d
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-03-01 23:59:26 +0900

    230301 23:58 Room!

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,3 +5,36 @@
 
 */
 const socket = io();
+
+/*
+    room?
+    socket IO에 내장되어 있는 기능으로, user가 website로 가면 방을 만들거나, 방에 참가할 수 있는 form을 보게 될 것이다.
+    간단하게 방에 참가하거나 떠나는 것을 구현 가능하다. 또한 방에 메시지를 보내는 것 등등 다양한 기능 구현을 간단하게 할 수 있다.
+*/
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+
+function backendDone(msg){
+    alert(`server is done! the backend says: `, msg);
+}
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room",{payload:input.value}, 1, "hello", true, input.value, backendDone)
+                                            //메세지를 send할 필요 없이 특정한 어떤 event던지 emit해주면 서버로 전송 할 수 있다.
+                                            //그리고 두번째 argument로 어떤 타입으로든 보낼 수 있고, 한가지만 보내야한다는 제약도 없다.(websocket은 text만 가능)
+                                            //그리고 **마지막** argument에는 서버에서 function(callback function)을 호출해서 프론트에서 해당 함수를 실행할 수 있다.
+                                            // 마지막 argument에 콜백함수를 사용할 때, 절대!! 백엔드서버에서 함수가 실행되도록 하면 안된다. 보안에 심각한 문제가 생길 수도 있다.
+                                            // 이 모든 것들을 emit만 해주면 socketIO가 알아서 핸들링 해준다.
+                                            /*
+                                                *websocket vs socket.io
+                                                websocket은 message라는 event를 사용했어야 하는데, socketio는 원하는 이벤트를 서버에 전달 할 수있게 되었다.
+                                                websocket은 string type의 data만 전송 가능 했는데, socketIO는 object 형식으로 전달 가능해졌다.(socketIO의 핸들링 덕분)
+                                            */
+    input.value = ""
+}
+
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -14,9 +14,21 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer); //socket.io를 설치해주는 것 만으로 url/socket.io/socket.io.js를 제공해준다.
                                        //그 이유는 socketIO는 websocket의 부가기능이 아니기 때문에,  websocket을 사용할 수 없을때 이것들을 사용해 도와준다.(재연결 같은 기능들..)
                                        // go to ==> "home.pug"
-wsServer.on("connection", socket => {
-    console.log(socket); // **websocket이 아닌 socketIO의 socket**으로 connection 받을 준비 완료.
-})
+
+wsServer.on("connection", (socket) => {
+    // console.log(socket); // **websocket이 아닌 socketIO의 socket**으로 connection 받을 준비 완료, 또한 서버가 다운되면 자동으로 재연결을 계속 시도한다.
+    socket.on("enter_room", (objectType,numberType,stringType,booleanType, roomName, done) => {
+        console.log(objectType);
+        console.log(numberType);
+        console.log(stringType);
+        console.log(booleanType);
+        console.log(roomName);
+
+        setTimeout(()=> { //주로 처리 비용이 크고 시간이 오래 걸리는 작업을 백엔드서버에서 완료 후 프론트에 전달하는 용도로 많이 사용한다.
+            done("hello from the backend"); // argument를 fromt에 전달 할 수도 있다.
+        }, 10000);
+    });
+});
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
